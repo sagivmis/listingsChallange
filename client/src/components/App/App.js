51dc1e30001b5569c5a94694d609240da3c70998
Sagiv Mishaan <sagiv505@gmail.com>
2021-10-09 00:04:42 +0300

    Initial

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,27 +1,33 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [exampleRes, setExampleRes] = useState();
+    const [exampleRes, setExampleRes] = useState();
 
-  const fetchData = async () => {
-    try {
-      const { message } = await axios.get('http://localhost:8080/example').then(({ data }) => data);
-      setExampleRes(message);
-    } catch (err) { console.log(err); }
-  };
+    const fetchData = async () => {
+        try {
+            const { message } = await axios
+                .get("http://localhost:8080/example")
+                .then(({ data }) => data);
+            setExampleRes(message);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-  useEffect(() => { fetchData(); });
+    useEffect(() => {
+        fetchData();
+    });
 
-  return (
-    <div className="App">
-      <header />
-      <div className="container">
-        <h1> {exampleRes} </h1>
-      </div>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header />
+            <div className='container'>
+                <h1> {exampleRes} </h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
